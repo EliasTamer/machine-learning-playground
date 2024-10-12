53a549b43487d99f1f8a6432add7bcf29cdbc798
Elias Tamer <eliastamer3@gmail.com>
2024-10-12 11:00:00 +0300

    added model evaluations and diagnosis

--- supervised-learning/neural_networks/model_evaluation.py
+++ supervised-learning/neural_networks/model_evaluation.py
@@ -1,8 +1,17 @@
+# for array computations and loading data
 import numpy as np
+
+# for building linear regression models and preparing data
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import StandardScaler, PolynomialFeatures
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
-from sklearn.preprocessing import StandardScaler, PolynomialFeatures
-from sklearn.linear_model import LinearRegression
+
+# for building neural networks
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+
 
 x = np.array([[1651.  ], [1691.82], [1732.63], [1773.45],[1814.27], [1855.08], [1895.9 ],
  [1936.71],
@@ -107,45 +116,43 @@ x_train, x_, y_train, y_ = train_test_split(x, y, test_size=0.40, random_state=1
 # split the 40% subset above into two: one half for cross validation and the other for the test set
 x_cv, x_test, y_cv, y_test = train_test_split(x_, y_, test_size=0.50, random_state=1)
 
-# initialize lists to save the errors, models, and feature transforms
-train_mses = []
-cv_mses = []
-models = []
-polys = []
-scalers = []
 
-# loop over 10 times. Each adding one more degree of polynomial higher than the last.
-for degree in range(1,11):
+def build_models():
+    tf.random.set_seed(20)
     
-    # add polynomial features to the training set
-    poly = PolynomialFeatures(degree, include_bias=False)
-    X_train_mapped = poly.fit_transform(x_train)
-    polys.append(poly)
+    model_1 = Sequential(
+        [
+            Dense(25, activation = 'relu'),
+            Dense(15, activation = 'relu'),
+            Dense(1, activation = 'linear')
+        ],
+        name='model_1'
+    )
+
+    model_2 = Sequential(
+        [
+            Dense(20, activation = 'relu'),
+            Dense(12, activation = 'relu'),
+            Dense(12, activation = 'relu'),
+            Dense(20, activation = 'relu'),
+            Dense(1, activation = 'linear')
+        ],
+        name='model_2'
+    )
+
+    model_3 = Sequential(
+        [
+            Dense(32, activation = 'relu'),
+            Dense(16, activation = 'relu'),
+            Dense(8, activation = 'relu'),
+            Dense(4, activation = 'relu'),
+            Dense(12, activation = 'relu'),
+            Dense(1, activation = 'linear')
+        ],
+        name='model_3'
+    )
     
-    # scale the training set
-    scaler_poly = StandardScaler()
-    X_train_mapped_scaled = scaler_poly.fit_transform(X_train_mapped)
-    scalers.append(scaler_poly)
+    model_list = [model_1, model_2, model_3]
     
-    # create and train the model
-    model = LinearRegression()
-    model.fit(X_train_mapped_scaled, y_train )
-    models.append(model)
-    
-    # compute the training MSE
-    yhat = model.predict(X_train_mapped_scaled)
-    train_mse = mean_squared_error(y_train, yhat) / 2
-    train_mses.append(train_mse)
-    
-    # add polynomial features and scale the cross validation set
-    X_cv_mapped = poly.transform(x_cv)
-    X_cv_mapped_scaled = scaler_poly.transform(X_cv_mapped)
-    
-    # compute the cross validation MSE
-    yhat = model.predict(X_cv_mapped_scaled)
-    cv_mse = mean_squared_error(y_cv, yhat) / 2
-    cv_mses.append(cv_mse)
-    
-# you can decide to use the model with the lowest cv_mse as the one best suited for your application.
-degree = np.argmin(cv_mses) + 1
-print(f"Lowest CV MSE is found in the model with degree={degree}")
+    return model_list
+
