816b08770e7332b0b1b4eec9f0f8c8594f70fb9b
Elias Tamer <eliastamer3@gmail.com>
2024-11-01 16:35:16 +0200

    replaced my py files with well documented ipynb examples of eveyr learning algorithm

--- supervised-learning/neural_networks/index.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-from custom_neural_network import my_dense
-
-W1_tmp = np.array( [[-8.93,  0.29, 12.9 ], [-0.1,  -7.32, 10.81]] )
-b1_tmp = np.array( [-9.82, -9.28,  0.96] )
-W2_tmp = np.array( [[-31.18], [-27.59], [-32.56]] )
-b2_tmp = np.array( [15.41] )
-
